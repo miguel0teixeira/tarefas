20cb4f02ed7bb2b42f781c1f7b3ae8e1ed3108ac
Miguel Teixeira <miguel.teixeira2669@gmail.com>
2025-05-31 21:27:33 -0300

    quinto commit

---
+++ quinto.py
@@ -0,0 +1,6 @@
+despesas = int(input("digite a sua quantidade de gastos esse mês (R$): "))
+
+if despesas > 3000.00:
+    print("passou do limite de despesas esse mês -3.000-")
+else:
+    print("ainda está dentro do seu limite de 3000")
